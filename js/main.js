a39e68a796a2aed5d1756cfc59eaba0996db234c
zarif-taktashev <zarifbeta@gmail.com>
2020-02-14 20:10:37 +0600

    module3-task2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 'use strict';
 
-function createOffer (index) {
-  var address = ['100, 350', '150, 350', '100, 300', '500, 310', '400, 310', '360, 300', '90, 350', '260, 350'];
-  var locations = [{x: 72, y: 140}, {x: 87, y: 140}, {x: 65, y: 140}, {x: 63, y: 180}, {x: 50, y: 180}, {x: 26, y: 630}, {x: 20, y: 600}, {x: 10, y: 550}];
+function createOffer(i) {
+  var avatarNumber = i < 10 ? '0' + (i + 1) : i + 1;
   var Offer =
   {
     'author': {
-      'avatar': 'img/avatars/user0' + index + '.png'
+      'avatar': 'img/avatars/user' + avatarNumber + '.png'
     },
     'offer': {
-      'title': 'Title ' + index,
-      'address': address[i - 1],
+      'title': 'Title ' + i,
+      'address': address[i],
       'price': 100,
       'type': 'palace',
       'rooms': 2,
@@ -22,8 +21,8 @@ function createOffer (index) {
       'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg'],
     },
     'location': {
-      'x': locations[i - 1].x,
-      'y': locations[i - 1].y
+      'x': locations[i].x,
+      'y': locations[i].y
     }
   };
 
@@ -33,20 +32,22 @@ function createOffer (index) {
 document.querySelector('.map').classList.remove('map--faded');
 var pins = document.querySelector('.map__pins');
 var elements = document.querySelector('#pin').content.querySelector('.map__pin');
-var fragment = document.createDocumentFragment();
 
-function renderPin(elem) {
-  var element = elements.cloneNode(true);
-  element.querySelector('img').alt = elem.offer.title;
-  element.querySelector('img').src = elem.author.avatar;
-  element.style.top = +elem.offer.address.split(',')[0] + elem.location.x + 'px';
-  element.style.left = +elem.offer.address.split(',')[1] + elem.location.y + 'px';
-  return element;
+var address = ['100, 350', '150, 350', '100, 300', '500, 310', '400, 310', '360, 300', '90, 350', '260, 350'];
+var locations = [{x: 72, y: 140}, {x: 87, y: 140}, {x: 65, y: 140}, {x: 63, y: 180}, {x: 50, y: 180}, {x: 26, y: 630}, {x: 20, y: 600}, {x: 10, y: 550}];
+
+function addPins() {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < 8; i++) {
+    var elem = createOffer(i);
+    var element = elements.cloneNode(true);
+    element.querySelector('img').alt = elem.offer.title;
+    element.querySelector('img').src = elem.author.avatar;
+    element.style.top = +elem.offer.address.split(',')[0] + elem.location.x + 'px';
+    element.style.left = +elem.offer.address.split(',')[1] + elem.location.y + 'px';
+    fragment.appendChild(element);
+  }
+  return fragment;
 }
 
-for (var i = 1; i < 8; i++) {
-  var elem = createOffer(i);
-  fragment.appendChild(renderPin(elem));
-}
-
-pins.appendChild(fragment);
+pins.appendChild(addPins());
